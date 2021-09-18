fbc443e2efbc99bda5bf5a365272d1d46699d255
theart84 <artshadow74@gmail.com>
2021-09-18 14:25:18 +0500

    38_lesson

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Post, HttpCode, Body } from '@nestjs/common';
+import { Controller, Post, HttpCode, Body, UsePipes, ValidationPipe, BadRequestException } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
+import { ALREADY_REGISTERED_ERROR } from './auth.constants';
 
 @Controller('auth')
 export class AuthController {
-  @Post('register')
-  async register(@Body() dto: AuthDto) {}
+	constructor(private readonly authService: AuthService) {
+	}
 
-  @HttpCode(200)
-  @Post('login')
-  async login(@Body() dto: AuthDto) {}
+	@UsePipes(new ValidationPipe())
+	@Post('register')
+	async register(@Body() dto: AuthDto) {
+		const oldUser = await this.authService.findUser(dto.login);
+		if (oldUser) {
+			throw new BadRequestException(ALREADY_REGISTERED_ERROR);
+		}
+		return this.authService.createUser(dto);
+	}
+
+	@HttpCode(200)
+	@Post('login')
+	async login(@Body() dto: AuthDto) {
+	}
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { UserModel } from './user.model';
+import { AuthService } from './auth.service';
 
 @Module({
-  controllers: [AuthController],
-  imports: [
-	TypegooseModule.forFeature([
-		{
-		typegooseClass: AuthModule,
-		schemaOptions: {
-			collection: 'Auth',
-		},
-		},
-	]),
-  ],
+	controllers: [AuthController],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: UserModel,
+				schemaOptions: {
+					collection: 'User',
+				},
+			},
+		]),
+	],
+	providers: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule {
+}
