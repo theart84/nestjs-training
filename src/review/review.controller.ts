afa6050ca1ca05ee323fbf442b2183fe9e72fbaf
theart84 <artshadow74@gmail.com>
2021-09-18 15:37:04 +0500

    41_lesson

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,40 +1,43 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Param,
-  Post,
-  Get,
-  HttpException,
-  HttpStatus,
-  UsePipes,
-  ValidationPipe,
+	Body,
+	Controller,
+	Delete,
+	Param,
+	Post,
+	Get,
+	HttpException,
+	HttpStatus,
+	UsePipes,
+	ValidationPipe, UseGuards,
 } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Controller('review')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {
-  }
+	constructor(private readonly reviewService: ReviewService) {
+	}
 
-  @UsePipes(new ValidationPipe())
-  @Post('create')
-  async create(@Body() dto: CreateReviewDto) {
-	  return this.reviewService.create(dto);
-  }
+	@UsePipes(new ValidationPipe())
+	@Post('create')
+	async create(@Body() dto: CreateReviewDto) {
+		return this.reviewService.create(dto);
+	}
 
-  @Delete(':id')
-  async delete(@Param('id') id: string) {
-    const deletedDoc = await this.reviewService.delete(id);
-    if (!deletedDoc) {
-      throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
-    }
-  }
 
-  @Get('byProduct/:productId')
-  async getByProduct(@Param('productId') productId: string) {
-	  return this.reviewService.findByProductId(productId);
-  }
+	@UseGuards(JwtAuthGuard)
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		const deletedDoc = await this.reviewService.delete(id);
+		if (!deletedDoc) {
+			throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@Get('byProduct/:productId')
+	async getByProduct(@Param('productId') productId: string) {
+		return this.reviewService.findByProductId(productId);
+	}
 }
