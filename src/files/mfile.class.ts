d3201ca9d0b29044a45a99c9c5234425fd125143
theart84 <artshadow74@gmail.com>
2021-09-19 14:33:37 +0500

    60_lesson

---
+++ src/files/mfile.class.ts
@@ -0,0 +1,10 @@
+
+export class MFile {
+	originalname: string;
+	buffer: Buffer;
+
+	constructor(file: Express.Multer.File | MFile) {
+		this.originalname = file.originalname;
+		this.buffer = file.buffer;
+	}
+}
